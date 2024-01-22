62e7790d2b37605dc7b38bb3a584276c86425b19
dpk667 <pashadereza@gmail.com>
2024-01-22 09:36:23 +0300

    initialized project

---
+++ src/main/java/dpk/t1_consulting_testtask/T1ConsultingTestTaskApplication.java
@@ -0,0 +1,13 @@
+package dpk.t1_consulting_testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1ConsultingTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T1ConsultingTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dpk/t1_consulting_testtask/T1ConsultingTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package dpk.t1_consulting_testtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1ConsultingTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
